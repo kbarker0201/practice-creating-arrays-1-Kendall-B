fa5d18f59f8f79f019bd31db5bcccc63832fc59b
kbarker0201 <kbarker0201@gmail.com>
2025-07-20 19:32:58 -0500

    final edits

--- practice.js
+++ practice.js
@@ -1,1 +1,8 @@
-console.log();
+let arrGroceries = ["Apples", "Oranges", "Lettuce", "Peanut Butter", "Cheese"];
+let arrPrices = [3.99, 4.99, 1.29];
+
+console.log(arrGroceries);
+console.log(arrPrices);
+
+console.log(arrGroceries[2]);
+console.log(arrPrices[3]);
